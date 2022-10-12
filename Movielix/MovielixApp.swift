84e2c08b6666f44b98519c0a9f19f8a34c97b8d7
ferdogan <fatih.erdogan@innology.com.tr>
2022-10-12 10:48:27 +0300

    Initial Commit

---
+++ Movielix/MovielixApp.swift
@@ -0,0 +1,17 @@
+//
+//  MovielixApp.swift
+//  Movielix
+//
+//  Created by Fatih Erdoğan on 12.10.2022.
+//
+
+import SwiftUI
+
+@main
+struct MovielixApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
